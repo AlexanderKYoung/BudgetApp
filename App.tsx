2ab181f2a21fe224bf66dcee7da3d4d7f63c8ff1
Alexander Young <57117011+AlexanderKYoung@users.noreply.github.com>
2023-10-01 17:16:46 -0500

    Boilerplate set up

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { COLORS, icons, images, SIZES } from "./constants/constants";
 
 export default function App() {
   return (
---
+++ constants/theme.tsx
@@ -0,0 +1,31 @@
+const COLORS = {
+    primary: "",
+    secondary: "",
+    tertiary: "",
+
+    gray: "",
+    gray2: "",
+    
+    white: "",
+    white2: ""
+};
+
+const FONT = {
+    regular: "",
+    medium: "",
+    bold: ""
+};
+
+const SIZES = {
+    xSmall: 10,
+    small: 12,
+    medium: 16,
+    large: 20,
+    xLarge: 24
+};
+
+const SHADOWS = {
+
+};
+
+export { COLORS, FONT, SIZES, SHADOWS };
